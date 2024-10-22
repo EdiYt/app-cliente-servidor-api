7773e9b8e411268dacb116ccfeb16dc52892e184
EdiYt <erickvillaseor@yahoo.com.mx>
2024-10-21 23:34:29 -0600

    Se subio el getAllPublico para que no interfiera con otra ruta

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
+// Ruta para obtener todos los libros con nombres modificados (getAllPublico)
+router.get('/publico', bookController.getAllPublico);
+
 // Ruta para obtener todos los libros
 router.get('/', bookController.getAllBooks);
 
 // Ruta para obtener un libro por ID
 router.get('/:id', bookController.getBookById);
-
-// GetAll publico
-router.get('/publico', bookController.getAllPublico);
 
 // Ruta para crear un nuevo libro
 router.post('/', bookController.createBook);
@@ -17,5 +17,4 @@ router.post('/', bookController.createBook);
 // Ruta para actualizar un libro
 router.put('/:id', bookController.updateBook);
 
-// Exportar el router para ser usado en el servidor principal
 module.exports = router;

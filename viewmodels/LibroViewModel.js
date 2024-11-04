923cc48c9c72f0da8e80d4965b6a9dde52ae95bd
EdiYt <erickvillaseor@yahoo.com.mx>
2024-11-04 14:19:23 -0600

    Se agrego el campo universidad al mapeo

--- viewmodels/LibroViewModel.js
+++ viewmodels/LibroViewModel.js
@@ -11,6 +11,7 @@ class BooksViewModel {
             nombre: book.nombre,
             autor: book.autor,
             genero: book.genero,
+            universidad: book.universidad, 
             pdfPath: book.pdf_path,
             estatus: book.estatus ? 'Disponible' : 'No disponible',
         };
